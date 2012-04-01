b86458e11ffdc750ee8e356effd97896f3bc749f
Richard Kreckel <kreckel@ginac.de>
2012-04-01 22:38:54 +0200

    Detect s390x and sparc64 as 32-bit architectures.
    CLN fails to build on s390x and sparc64 as it detects them as 32-bit
    architectures, due to __s390__ and __sparc__ matching respectively both
    s390 and s390x, and both sparc and sparc64. The patch below fixes the
    issue. (Reported as Debian bug#639494.)
    
    Thanks to Aurelien Jarno <aurel32@debian.org>.

--- include/cln/object.h
+++ include/cln/object.h
@@ -22,10 +22,10 @@ namespace cln {
 #if defined(__m68k__)
   #define cl_word_alignment  2
 #endif
-#if defined(__i386__) || defined(__mips__) || defined(__mipsel__) || (defined(__sparc__) && !defined(__sparc64__)) || defined(__hppa__) || defined(__arm__) || defined(__rs6000__) || defined(__m88k__) || defined(__convex__) || defined(__s390__) || defined(__sh__)
+#if defined(__i386__) || defined(__mips__) || defined(__mipsel__) || (defined(__sparc__) && !defined(__arch64__)) || defined(__hppa__) || defined(__arm__) || defined(__rs6000__) || defined(__m88k__) || defined(__convex__) || (defined(__s390__) && !defined(__s390x__)) || defined(__sh__)
   #define cl_word_alignment  4
 #endif
-#if defined(__alpha__) || defined(__ia64__) || defined(__mips64__) || defined(__powerpc64__) || defined(__sparc64__) || defined(__x86_64__)
+#if defined(__alpha__) || defined(__ia64__) || defined(__mips64__) || defined(__powerpc64__) || (defined(__sparc__) && defined(__arch64__)) || defined(__x86_64__) || defined(__s390x__)
   #define cl_word_alignment  8
 #endif
 #if !defined(cl_word_alignment)
